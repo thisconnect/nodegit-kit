21dc229397c2968aaad39c545432dcc02655055c
thisconnect <hi@this-connect.me>
2015-12-17 21:59:13 +0100

    test get (internal)

--- test/test.js
+++ test/test.js
@@ -4,5 +4,6 @@ debug.log = console.log.bind(console);
 require('./test-init.js');
 require('./test-open.js');
 require('./test-commits.js');
+require('./test-get.js');
 require('./test-state.js');
 require('./test-config.js');
