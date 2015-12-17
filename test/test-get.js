14c4808506167c9cf01731f9f378f2e2f7721b95
thisconnect <hi@this-connect.me>
2015-12-17 22:00:45 +0100

    tests internal get

---
+++ test/test-get.js
@@ -0,0 +1,99 @@
+var git = require('../');
+var get = require('../lib/get.js');
+
+var tape = require('tape');
+var files = require('fildes-extra');
+var resolve = require('path').resolve;
+
+var dir = resolve(__dirname, 'repos/get');
+var file = resolve(dir, 'file.txt');
+
+
+tape('get setup', function(t){
+    files.rmdir(dir)
+    .then(function(){
+        return files.write(file, '1\n');
+    })
+    .then(function(){
+        return git.open(dir);
+    })
+    .then(function(repo){
+        return files.appendFile(file, '2\n')
+        .then(function(){
+            return git.commit(repo);
+        })
+        .then(function(){
+            return files.appendFile(file, '3\n');
+        })
+        .then(function(){
+            return git.commit(repo);
+        })
+        .then(function(){
+            return files.appendFile(file, '4\n');
+        })
+        .then(function(){
+            return git.commit(repo, {
+                message: 'last commit'
+            });
+        });
+    })
+    .then(function(){
+        t.end();
+    })
+    .catch(function(err){
+        t.error(err);
+        t.end();
+    });
+});
+
+tape('test get', function(t){
+
+    git.open(dir)
+    .then(function(repo){
+        return repo.getBranchCommit('master')
+        .then(function(commit){
+            return new Promise(function(resolve, reject){
+                commit.history()
+                .on('end', resolve)
+                .on('error', reject)
+                .start();
+            });
+        })
+        .then(function(commits){
+            return Promise.all(commits.map(function(commit){
+                return commit.sha();
+            }));
+        })
+        .then(function(commits){
+            return get(repo)
+            .then(function(commit){
+                t.equal(commit.sha(), commits[0], 'got last commit');
+            })
+            .then(function(){
+                return get(repo, commits[3])
+                .then(function(commit){
+                    t.equal(commit.sha(), commits[3], 'got first commit');
+                });
+            })
+            .then(function(){
+                return get(repo, commits[1].slice(0, 7))
+                .then(function(commit){
+                    t.equal(commit.sha(), commits[1], 'got abbrev commit');
+                });
+            })
+            .then(function(){
+                return get(repo, 'HEAD~2')
+                .then(function(commit){
+                    t.equal(commit.sha(), commits[2], 'got second last commit');
+                });
+            });
+        });
+    })
+    .then(function(){
+        t.end();
+    })
+    .catch(function(err){
+        t.error(err);
+        t.end();
+    });
+});
